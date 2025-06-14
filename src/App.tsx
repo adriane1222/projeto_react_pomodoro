76378d73586309ccd62735248806d9147fa8083e
Adriane Mesquita <drih.mesquita@hotmail.com>
2025-06-14 20:36:15 -0300

    css modules escopo do componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 //PascalCase
 //ExemploDeComponente
+import { Heading } from './components/Heading'
+import './styles/global.css'
+import './styles/theme.css'
 
-import { Fragment } from "react/jsx-runtime";
 
 export function App() {
-    console.log('Olár')
-
     return (
-        <Fragment> 
-            <h1>Olá mundo</h1>
+        <> 
+           <Heading/> 
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat est ea saepe quasi ad error at? Exercitationem, consequatur iusto! Quisquam qui, voluptatum tempore earum in
                 optio vero ad velit tempora?
             </p>
-        </Fragment>
+        </>
     ); // com fragment ou < > </> vazio
 }
